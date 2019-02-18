d93a0f5ce5ec09bbd1be0b259f22ed4ccd4a4f93
smithumble <smithumble@gmail.com>
2019-02-18 20:21:26 +0200

    Open files with UTF-8 by default

--- contextmanagers.py
+++ contextmanagers.py
@@ -3,13 +3,13 @@ import os
 
 
 @contextmanager
-def open_file(path, mode='r', **kwargs):
+def open_file(path, mode='r', encoding='UTF-8', **kwargs):
     try:
         this_dir, this_filename = os.path.split(__file__)
         data_path = os.path.join(this_dir, "data", path)
-        file = open(data_path, mode, **kwargs)
+        file = open(data_path, mode, encoding=encoding, **kwargs)
     except IOError:
-        file = open(path, mode, **kwargs)
+        file = open(path, mode, encoding=encoding, **kwargs)
     yield file
     file.close()
 
