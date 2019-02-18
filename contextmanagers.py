96474f83b3b638aece69564ad69957e82aea7d4e
smithumble <smithumble@gmail.com>
2019-02-18 10:45:16 +0200

    Remove print from contextmanagers

--- contextmanagers.py
+++ contextmanagers.py
@@ -7,7 +7,6 @@ def open_file(path, mode='r', **kwargs):
     try:
         this_dir, this_filename = os.path.split(__file__)
         data_path = os.path.join(this_dir, "data", path)
-        print(data_path)
         file = open(data_path, mode, **kwargs)
     except IOError:
         file = open(path, mode, **kwargs)
