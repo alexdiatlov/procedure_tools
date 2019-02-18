03cb7e56829bf95300c4190364f7bb11dc746412
smithumble <smithumble@gmail.com>
2019-02-18 19:37:50 +0200

    Add requests to setup requires

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from version import __version__
 setup(
     name="procedure_tools",
     version=__version__,
+    install_requires=[
+        'requests',
+    ],
     entry_points={
         'console_scripts': [
             'procedure=procedure:main',
