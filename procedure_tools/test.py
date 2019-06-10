cbeabc352218862ac0e2999349fe2944ee29912b
smithumble <smithumble@gmail.com>
2019-06-10 15:36:38 +0300

    Leave only one procedure method for tests

--- procedure_tools/test.py
+++ procedure_tools/test.py
@@ -12,19 +12,19 @@ from procedure_tools.procedure import main
     reason='Either HOST or TOKEN env variables not specified'
 )
 @pytest.mark.parametrize("argv", [
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdEU"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdUA"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdUA.defense"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "belowThreshold"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "closeFrameworkAgreementUA"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "competitiveDialogueEU"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "competitiveDialogueUA"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "esco"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "negotiation"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "negotiation.quick"]),
     (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "reporting"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--stop", "bid_create_0.json"]),
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--wait", "edr-qualification"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "negotiation"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "negotiation.quick"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "belowThreshold"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdEU"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdUA"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdUA.defense"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "closeFrameworkAgreementUA"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "competitiveDialogueEU"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "competitiveDialogueUA"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "esco"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--stop", "bid_create_0.json"]),
+    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--wait", "edr-qualification"]),
 ])
 def test_main(argv):
     host = os.environ.get('HOST')
