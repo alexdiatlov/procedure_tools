6cf8d3d9c13321ffecc64a5e21e5afe4a6e63aed
smithumble <smithumble@gmail.com>
2019-07-19 10:00:46 +0300

    Adjust tests params.

--- procedure_tools/test.py
+++ procedure_tools/test.py
@@ -12,27 +12,29 @@ from procedure_tools.procedure import main
     reason='Either HOST or TOKEN env variables not specified'
 )
 @pytest.mark.parametrize("argv", [
-    (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "reporting"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "negotiation"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "negotiation.quick"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "belowThreshold"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdEU"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdUA"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "aboveThresholdUA.defense"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "closeFrameworkAgreementUA"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "competitiveDialogueEU"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "competitiveDialogueUA"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--data", "esco"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--stop", "bid_create_0.json"]),
-    # (["--acceleration", "1000000", "--path", "/api/2.5/", "--wait", "edr-qualification"]),
+    (["--data", "reporting"]),
+    (["--data", "negotiation", "--stop", "tender_create.json"]),
+    (["--data", "negotiation.quick", "--stop", "tender_create.json"]),
+    (["--data", "belowThreshold", "--stop", "tender_create.json"]),
+    (["--data", "aboveThresholdEU", "--stop", "tender_create.json"]),
+    (["--data", "aboveThresholdEU.multilot", "--stop", "tender_create.json"]),
+    (["--data", "aboveThresholdUA", "--stop", "tender_create.json"]),
+    (["--data", "aboveThresholdUA.defense", "--stop", "tender_create.json"]),
+    (["--data", "closeFrameworkAgreementUA", "--stop", "tender_create.json"]),
+    (["--data", "competitiveDialogueEU", "--stop", "tender_create.json"]),
+    (["--data", "competitiveDialogueUA", "--stop", "tender_create.json"]),
+    (["--data", "esco", "--stop", "tender_create.json"]),
+    (["--wait", "edr-qualification"]),
 ])
 def test_main(argv):
     host = os.environ.get('HOST')
     token = os.environ.get('TOKEN')
+    
+    default_args = ["--acceleration", "1000000", "--path", "/api/2.5/"]
 
-    print("\n\nTest with args: %s\n\n" % ([host, "*" * 32] + argv))
+    print("\n\nTest with args: %s\n\n" % ([host, "*" * 32] + default_args + argv))
 
-    with mock.patch('sys.argv', ['', host, token] + argv), pytest.raises(SystemExit) as e:
+    with mock.patch('sys.argv', ['', host, token] + default_args + argv), pytest.raises(SystemExit) as e:
         main()
     assert e.type == SystemExit
     assert e.value.code == 0
