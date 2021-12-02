1ff7f0d4f9d1a4b08512a7559c7c2e51a2df3e39
smithumble <smithumble@gmail.com>
2021-12-02 12:10:52 +0200

    Refactor README.sh fake creds

--- README.sh
+++ README.sh
@@ -4,6 +4,10 @@ echo "Readme generation started."
 
 FILE='README.md'
 
+FAKE_API_TOKEN='broker_api_token'
+FAKE_DS_USERNAME='broker_ds_username'
+FAKE_DS_PASSWORD='broker_ds_password'
+
 cat > $FILE <<- EOM
 # procedure_tools
 
@@ -33,32 +37,32 @@ cat >> $FILE <<- EOM
 
 Create with default data
 \`\`\`
-procedure ${API_HOST} broker_api_token ${DS_HOST} broker_ds_username broker_ds_password --acceleration=1000000 --path=/api/0/ --data=closeFrameworkAgreementUA
+procedure ${API_HOST} ${FAKE_API_TOKEN} ${DS_HOST} ${FAKE_DS_USERNAME} ${FAKE_DS_PASSWORD} --acceleration=1000000 --path=/api/0/ --data=closeFrameworkAgreementUA
 \`\`\`
 
 Create with default data and stop after specific data file
 \`\`\`
-procedure ${API_HOST} broker_api_token ${DS_HOST} broker_ds_username broker_ds_password --acceleration=1000000 --path=/api/0/ --data=closeFrameworkAgreementUA
+procedure ${API_HOST} ${FAKE_API_TOKEN} ${DS_HOST} ${FAKE_DS_USERNAME} ${FAKE_DS_PASSWORD} --acceleration=1000000 --path=/api/0/ --data=closeFrameworkAgreementUA
 \`\`\`
 
 Create with custom data files (relative path)
 \`\`\`
-procedure ${API_HOST} broker_api_token ${DS_HOST} broker_ds_username broker_ds_password --acceleration=1000000 --path=/api/0/ --data=customdata/closeFrameworkAgreementUA
+procedure ${API_HOST} ${FAKE_API_TOKEN} ${DS_HOST} ${FAKE_DS_USERNAME} ${FAKE_DS_PASSWORD} --acceleration=1000000 --path=/api/0/ --data=customdata/closeFrameworkAgreementUA
 \`\`\`
 
 Create with custom data files (absolute path)
 \`\`\`
-procedure ${API_HOST} broker_api_token ${DS_HOST} broker_ds_username broker_ds_password --acceleration=1000000 --path=/api/0/ --data=/Users/JonhDoe/customdata/closeFrameworkAgreementUA
+procedure ${API_HOST} ${FAKE_API_TOKEN} ${DS_HOST} ${FAKE_DS_USERNAME} ${FAKE_DS_PASSWORD} --acceleration=1000000 --path=/api/0/ --data=/Users/JonhDoe/customdata/closeFrameworkAgreementUA
 \`\`\`
 
 Create with custom data files (absolute path, Windows)
 \`\`\`
-procedure ${API_HOST} broker_api_token ${DS_HOST} broker_ds_username broker_ds_password --acceleration=1000000 --path=/api/0/ --data=C:\Users\JonhDoe\customdata\closeFrameworkAgreementUA
+procedure ${API_HOST} ${FAKE_API_TOKEN} ${DS_HOST} ${FAKE_DS_USERNAME} ${FAKE_DS_PASSWORD} --acceleration=1000000 --path=/api/0/ --data=C:\Users\JonhDoe\customdata\closeFrameworkAgreementUA
 \`\`\`
 
 ## Output example
 \`\`\`
-procedure ${API_HOST} broker_api_token ${DS_HOST} broker_ds_username broker_ds_password --acceleration=1000000 --path=/api/0/ --data=closeFrameworkAgreementUA --stop=bid_create_4.json
+procedure ${API_HOST} ${FAKE_API_TOKEN} ${DS_HOST} ${FAKE_DS_USERNAME} ${FAKE_DS_PASSWORD} --acceleration=1000000 --path=/api/0/ --data=closeFrameworkAgreementUA --stop=bid_create_4.json
 \`\`\`
 \`\`\`
 EOM
@@ -76,32 +80,32 @@ cat >> $FILE <<- EOM
 
 \`\`\`
 export API_HOST=${API_HOST}
-export API_TOKEN=broker_api_token
+export API_TOKEN=${FAKE_API_TOKEN}
 export DS_HOST=${DS_HOST}
-export DS_USERNAME=broker_ds_username
-export DS_PASSWORD=broker_ds_password
+export DS_USERNAME=${FAKE_DS_USERNAME}
+export DS_PASSWORD=${FAKE_DS_PASSWORD}
 
 ./README.sh
 \`\`\`
 or
 \`\`\`
-API_HOST=${API_HOST} API_TOKEN=broker_api_token DS_HOST=${DS_HOST} DS_USERNAME=broker_ds_username DS_PASSWORD=broker_ds_password ./README.sh
+API_HOST=${API_HOST} API_TOKEN=${FAKE_API_TOKEN} DS_HOST=${DS_HOST} DS_USERNAME=${FAKE_DS_USERNAME} DS_PASSWORD=${FAKE_DS_PASSWORD} ./README.sh
 \`\`\`
 
 ## Run tests
 
 \`\`\`
 export API_HOST=${API_HOST}
-export API_TOKEN=broker_api_token
+export API_TOKEN=${FAKE_API_TOKEN}
 export DS_HOST=${DS_HOST}
-export DS_USERNAME=broker_ds_username
-export DS_PASSWORD=broker_ds_password
+export DS_USERNAME=${FAKE_DS_USERNAME}
+export DS_PASSWORD=${FAKE_DS_PASSWORD}
 
 python setup.py test
 \`\`\`
 or
 \`\`\`
-API_HOST=${API_HOST} API_TOKEN=broker_api_token DS_HOST=${DS_HOST} DS_USERNAME=broker_ds_username DS_PASSWORD=broker_ds_password python setup.py test
+API_HOST=${API_HOST} API_TOKEN=${FAKE_API_TOKEN} DS_HOST=${DS_HOST} DS_USERNAME=${FAKE_DS_USERNAME} DS_PASSWORD=${FAKE_DS_PASSWORD} python setup.py test
 \`\`\`
 EOM
 
