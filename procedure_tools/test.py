9e08fad9bb21b075512ddd6a45b4aca3db66d571
smithumble <smithumble@gmail.com>
2024-08-01 09:46:27 +0300

    Fix formatting

--- procedure_tools/test.py
+++ procedure_tools/test.py
@@ -27,7 +27,14 @@ skipifenv = pytest.mark.skipif(
 
 
 def run_test(argv):
-    default_args = ["--acceleration", "10000", "--path", "/api/2.5/", "--submission", "quick(mode:no-auction)"]
+    default_args = [
+        "--acceleration",
+        "10000",
+        "--path",
+        "/api/2.5/",
+        "--submission",
+        "quick(mode:no-auction)",
+    ]
     args = (
         [
             os.environ.get(API_HOST),
